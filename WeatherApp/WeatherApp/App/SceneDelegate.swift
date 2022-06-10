3ee04d60a3665ea4269066b8653486b76a7eea66
Arsenii Kovalenko <arsenkiy002@gmail.com>
2022-06-10 18:30:31 +0300

    Initial commit
    Base project was generated

---
+++ WeatherApp/WeatherApp/App/SceneDelegate.swift
@@ -0,0 +1,39 @@
+//
+//  SceneDelegate.swift
+//  WeatherApp
+//
+//  Created by Arsenii Kovalenko on 10.06.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        let appViewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: appViewController)
+        
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+    }
+}
---
+++ WeatherApp/WeatherApp/App/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Arsenii Kovalenko on 10.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+
+}
